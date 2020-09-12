d1b9405f5b4d1f1dc1dbc67ce9d04f362a61ff8b
Kiran Johns <kiranjohns369@gmail.com>
2020-09-12 13:00:05 +0530

    pannel cards

---
+++ src/components/Panel.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="panel">
+    <v-container>
+      <div class="suggestions">
+        <p class="title">Word Suggestions</p>
+        <template v-for="n in 4">
+          <v-row :key="n" align="start" justify="center">
+            <v-card class="card" outlined>
+                <v-btn depressed class="sug-id" color="#4C5DF5">{{n-1}}</v-btn>
+                <span class="sug-word">Word</span>
+            </v-card>
+          </v-row>
+        </template>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Panel"
+};
+</script>
+
+<style scoped>
+.panel {
+  padding-left: 5vw;
+  padding-right: 5vw;
+  padding-top: 2vh;
+}
+.title {
+  font-size: 20px;
+  text-align: center;
+  color: #4c5df5;
+  font-weight: 600;
+}
+.card{
+    text-align: left;
+    margin-left: 2rem;
+    margin-right: 2rem;
+    height: 3rem;
+    width:20rem;
+    margin-top:1rem;
+    border: solid 1px;
+    border-color: #4c5df5;
+}
+.suggestions{
+    margin: auto;
+    background:#F8F8F8;
+    padding-bottom: 1rem;
+    padding-top: 1rem;
+}
+.sug-id{
+  color: #fff;
+  font-weight: 700;
+  height: 3rem !important;
+  min-width: 3rem !important;
+}
+.sug-word{
+    padding-left: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <Navbar/>
+    <Panel/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
-
+import Panel from '@/components/Panel.vue'
 export default {
   name: 'Home',
   components: {
-    Navbar
+    Navbar,
+    Panel
   }
 }
 </script>
