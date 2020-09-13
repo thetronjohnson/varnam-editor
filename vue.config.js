222ddf11c5379bfdfe70b52961f8c7f0e7384475
Subin Siby <mail@subinsb.com>
2020-09-14 01:20:03 +0530

    Add env var for custom public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: typeof process.env.VUE_PUBLIC_PATH !== "undefined"
+    ? process.env.VUE_PUBLIC_PATH
+    : '/'
 }
