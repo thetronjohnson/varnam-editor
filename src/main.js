147255d1187982112dddc4a813a4a7e26d48e3f9
Subin Siby <mail@subinsb.com>
2020-09-14 01:27:58 +0530

    Set default API server url to be localhost

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-window.$VARNAM_API_URL = "https://api.varnamproject.com/"
+// api.varnamproject.com
+window.$VARNAM_API_URL = 'http://127.0.0.1:8089/'
 Vue.prototype.$VARNAM_API_URL = window.$VARNAM_API_URL
 
 Vue.prototype.$LANGS = {
