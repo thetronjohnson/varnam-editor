85d2e5e83601cf201495d0db63d7b4a4cd46af2d
Subin Siby <mail@subinsb.com>
2020-09-29 23:25:34 +0530

    Show import status on upload

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
+import VuetifyToast from 'vuetify-toast-snackbar-ng'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  components: {
+    VSnackbar,
+    VBtn,
+    VIcon
+  }
+})
+Vue.use(VuetifyToast)
 
 export default new Vuetify({
   theme: {
